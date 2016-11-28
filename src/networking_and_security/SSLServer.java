fe163f40109cd58aa90bd4fb65b36ad3dbc9f693
iliyan-kostov <>
2016-11-28 12:19:30 +0200

    Reworked "SSLServer.java". Additional functionality to be added.

--- src/networking_and_security/SSLServer.java
+++ src/networking_and_security/SSLServer.java
@@ -1,63 +1,96 @@
 package networking_and_security;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.net.ServerSocket;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLServerSocket;
 
 /**
- * THIS CLASS IS INCOMPLETE!
+ * THIS CLASS IS INCOMPLETE! - TO BE ADDED: LISTENING THREAD AND TASK MANAGER!
  *
- * This class is a factory for creating a multi-threaded SSL encrypted server
- * that uses a single thread for listening on the server port and accepting
- * incoming client connections. The server manages additional threads to handle
- * each authenticated client's incoming communication (one thread per client).
- * The communication is structured as a flow of messages. The processing of the
- * incoming messages is assigned to a single server-side message handler that
- * operates on a single shared task list. (to be added)
+ * This class is a factory for creating a multi-threaded server that uses SSL
+ * encryption. The server uses a single thread for listening on the server port
+ * and accepting incoming client connections. The server manages additional
+ * threads to handle each authenticated client's incoming communication (one
+ * thread per client). The communication is structured as a flow of messages.
+ * The processing of the incoming messages is assigned to a single server-side
+ * message handler that operates on a single shared task list. (to be added)
  *
  * @author iliyan-kostov
  */
 public class SSLServer {
 
     /**
-     * The socket that is used for accepting incoming client connections.
+     * the {@link SSLContext} that the server will use for network communication
+     * security.
      */
-    public SSLServerSocket sslServerSocket;
-
-    public SSLServer() {
-        this.sslServerSocket = null;
-    }
+    protected SSLContext sslContext;
 
     /**
-     * Returns the {@link SSLServer} object by creating a
-     * {@link SSLServerSocket} on the specified port, using a specified
-     * {@link SSLContext}. Exceptions are passed to the {@link ExceptionHandler}
-     * object.
+     * the {@link ServerSocket} that will be used for accepting incoming client
+     * connections. The socket will support and use SSL encryption.
+     */
+    protected ServerSocket serverSocket;
+
+    /**
+     * the local port that the server will operate on.
+     */
+    protected int port;
+
+    /**
+     * the {@link ExceptionHandler} object that the server will use to output
+     * logs or display messages.
+     */
+    protected ExceptionHandler exceptionHandler;
+
+    /**
+     * Initializes the {@link SSLServer} object fields according to the
+     * arguments provided, but does not start the server. To start the server
+     * the {@link #start()} function should be called. This allows port changing
+     * even if the {@link SSLServer} object is created but the server is not yet
+     * operative.
      *
      * @param sslContext the {@link SSLContext} for the server that describes
      * the server's private and public key pairs, the encryption mechanisms, the
      * trusted public keys of clients (certificates) and other
      * encryption-related features.
      *
-     * @param port the port that will be used to start the server on (the port
-     * to create the {@link SSLServerSocket}.
+     * @param port the local port that will be used to start the server on (the
+     * port to create the {@link ServerSocket}.
      *
      * @param exceptionHandler an object implementing the
      * {@link ExceptionHandler} interface, used to handle possible thrown
      * exceptions, for example to output a log or display a message to the user.
-     *
-     * @return
      */
-    public static SSLServer startServer(SSLContext sslContext, int port, ExceptionHandler exceptionHandler) {
-        SSLServer result = new SSLServer();
+    public SSLServer(SSLContext sslContext, int port, ExceptionHandler exceptionHandler) {
+        this.sslContext = sslContext;
+        this.port = port;
+        this.exceptionHandler = exceptionHandler;
+        this.serverSocket = null;
+    }
+
+    /**
+     * Starts the server by creating a {@link SSLServerSocket} on the specified
+     * port, using a specified {@link SSLContext}. Exceptions are passed to the
+     * {@link ExceptionHandler} object.
+     *
+     * @return true if successful, false otherwise.
+     */
+    public boolean start() {
+        boolean successful = true;
+
+        //create the server socket:
         try {
-            result.sslServerSocket = (SSLServerSocket) (sslContext.getServerSocketFactory().createServerSocket(port));
+            this.serverSocket = this.sslContext.getServerSocketFactory().createServerSocket(this.port);
         } catch (IOException ex) {
-            Logger.getLogger(SSLServer.class.getName()).log(Level.SEVERE, null, ex);
+            successful = false;
+            this.exceptionHandler.handle(ex);
+        }
+        if (!successful) {
+            return false;
         }
 
-        return null;
+        //initializes the task manager: TBA
+        //start the thread that listens for incoming client connection requests: TBA
+        return successful;
     }
 }
